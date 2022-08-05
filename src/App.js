ed365aedab07ca9982939f5f62c3ced1ad6070e9
timothy-tyk <timothytan94@gmail.com>
2022-08-05 10:40:30 +0800

    first commit, created form to submit messages to db

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
+import { database, storage } from "./firebase";
 import logo from "./logo.png";
 import "./App.css";
+import {
+  getDownloadURL,
+  ref as storageReference,
+  uploadBytes,
+} from "firebase/storage";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const MESSAGE_FOLDER_NAME = "messages";
+const IMAGES_FOLDER_NAME = "images";
 
 class App extends React.Component {
   constructor(props) {
@@ -13,7 +19,10 @@ class App extends React.Component {
     // Initialise empty messages array in state to keep local state in sync with Firebase
     // When Firebase changes, update local state, which will update local UI
     this.state = {
+      inputMessage: "",
       messages: [],
+      fileInputFile: null,
+      fileInputValue: "",
     };
   }
 
@@ -30,10 +39,35 @@ class App extends React.Component {
   }
 
   // Note use of array fields syntax to avoid having to manually bind this method to the class
-  writeData = () => {
+  writeData = (url) => {
+    let timeStamp = new Date().toLocaleString();
     const messageListRef = ref(database, MESSAGE_FOLDER_NAME);
     const newMessageRef = push(messageListRef);
-    set(newMessageRef, "abc");
+    set(newMessageRef, `${timeStamp}: ${this.state.inputMessage}`);
+  };
+
+  uploadImage = (e) => {
+    e.preventDefault();
+    console.log(
+      "uploading:",
+      `${IMAGES_FOLDER_NAME}/${this.state.fileInputFile.name}`
+    );
+    const storageRef = storageReference(
+      storage,
+      `${IMAGES_FOLDER_NAME}/${this.state.fileInputFile.name}`
+    );
+    uploadBytes(storageRef, this.state.fileInputFile)
+      .then((snapshot) => {
+        // console.log("uploaded:", snapshot);
+        // console.log(snapshot.ref)
+        return getDownloadURL(snapshot.ref);
+      })
+      .then((url) => {
+        console.log(url);
+        return this.writeData(url);
+      });
+
+    this.setState({ fileInputValue: "" });
   };
 
   render() {
@@ -49,7 +83,37 @@ class App extends React.Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           {/* TODO: Add input field and add text input as messages in Firebase */}
-          <button onClick={this.writeData}>Send</button>
+          <form>
+            <input
+              type="text"
+              onChange={(e) => this.setState({ inputMessage: e.target.value })}
+            />
+
+            <input
+              type="submit"
+              value="Send Message"
+              onClick={this.writeData}
+            />
+            <br />
+            <input
+              type="file"
+              value={this.state.fileInputValue}
+              onChange={(e) =>
+                this.setState({
+                  fileInputFile: e.target.files[0],
+                  fileInputValue: e.target.value,
+                })
+              }
+            />
+            <input
+              type="submit"
+              value="Upload"
+              onClick={(e) => {
+                this.uploadImage(e);
+              }}
+            />
+          </form>
+
           <ol>{messageListItems}</ol>
         </header>
       </div>
